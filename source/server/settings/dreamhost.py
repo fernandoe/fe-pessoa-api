8e80907063a56f6b92a0016127e56af2190ea72a
Fernando Espíndola <fer.esp@gmail.com>
2017-07-07 17:44:44 -0300

    WIP

--- source/server/settings/dreamhost.py
+++ source/server/settings/dreamhost.py
@@ -13,4 +13,10 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend'
 )
 
+OAUTH2_PROVIDER = {
+    'RESOURCE_SERVER_INTROSPECTION_URL': 'https://auth.fe.fernandoe.com/api/v1/introspect/',
+    'RESOURCE_SERVER_AUTH_TOKEN': 'fe-pessoa-server',
+    'OAUTH2_VALIDATOR_CLASS': 'fe_core.services.auth.oauth2_validators.FEOAuth2Validator'
+}
+
 STATIC_ROOT = '/home/fems/pessoa.fe.fernandoe.com/public/static'
