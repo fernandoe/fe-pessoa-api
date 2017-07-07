48b2561485e97d2d62f069c2a86b5985f3fe6113
Fernando Espíndola <fer.esp@gmail.com>
2017-07-07 17:49:58 -0300

    WIP

--- source/server/settings/dreamhost.py
+++ source/server/settings/dreamhost.py
@@ -2,12 +2,6 @@ from .base import *
 
 ALLOWED_HOSTS = ['*']
 
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'oauth2_provider.contrib.rest_framework.authentication.OAuth2Authentication',
-    )
-}
-
 AUTHENTICATION_BACKENDS = (
     'oauth2_provider.backends.OAuth2Backend',
     'django.contrib.auth.backends.ModelBackend'
