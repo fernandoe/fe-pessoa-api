1c7ab931270662306b8a7dda97a26f0420f69f12
Fernando Espíndola <fernando.espindola@hp.com>
2018-04-27 19:01:52 -0300

    add some changes to the admin page

--- src/fe_pessoa/admin.py
+++ src/fe_pessoa/admin.py
@@ -1,4 +1,3 @@
-# -*- coding:utf-8 -*-
 from django.contrib import admin
 
 from fe_pessoa.models import Cliente
@@ -6,9 +5,9 @@ from fe_pessoa.models import Cliente
 
 @admin.register(Cliente)
 class ClienteModelAdmin(admin.ModelAdmin):
-    search_fields = ('nome', 'cpf',)
+    search_fields = ('uuid', 'nome', 'cpf',)
     list_filter = ['transiente']
-    list_display = ('get_uuid', 'entidade', 'nome', 'email', 'telefone_celular', 'ativo', 'transiente')
+    list_display = ('get_uuid', 'entidade', 'nome', 'email', 'telefone_celular', 'ativo', 'transiente', 'endereco')
     ordering = ('nome',)
 
     def save_model(self, request, obj, form, change):
